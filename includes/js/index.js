645b092d03a6ea0fe7e34497c1f81d8277c2e465
root <pintea.sorin2007@gmail.com>
2014-10-06 18:18:26 +0300

    To be continued ..

--- includes/js/index.js
+++ includes/js/index.js
@@ -1,32 +1,58 @@
-var App = angular.module("App",[ ]);
+var game = angular.module("ics_zero",[]);
 
-App.controller("ctrl_app1",['$scope','srv', function($scope,srv) {
-	$scope.srv = srv;
-	$scope.testService = function() {
-		srv.increment();
+game.factory('tiles',[ function() {
+	var tiles = [];
+	for ( i = 0 ; i < 9 ; i ++ ) {
+		tiles.push({
+			number: i,
+			player: -1
+			});
 	}
+	return tiles;
 }]);
 
-App.controller("ctrl_app2",['$scope','srv',function($scope,srv){
-	$scope.anotherNumber = srv;
-	$scope.testAgain = function() {
-		srv.increment();
-	}
-}]);
-// create a service (acts like a singleton everywhere is used )
-App.factory("srv", function() {
-	// return an object with a getServiceName function
+game.factory('players',[ function() {
 	return {
-		number : 10,
-		increment : function() {
-			this.number++;
-		},
-		getServiceName: function() {
-			return "serviciu de test";
+        totalPlayers: [{
+					playerNumber : 1,
+					playerSimbol : 'X'
+				},
+				{
+					playerNumber : 2,
+					playerSimbol : '0'
+				}],
+		currentPlayer : 1
+        };
+    }]);
+
+// This shit maganes player turns 
+game.controller("player-controller",["$scope", "players", function($scope, players) {
+	$scope.players = players.totalPlayers;
+	// @TO-DO : on event thrown at line46, execute function
+	$scope.nextPlayer = function() {
+		players.currentPlayer = ( players.currentPlayer == 2 ) ? 1 : 2;
+	}
+}]);
+
+// This shit represents the board on which you can play
+game.controller("board-controller",["$scope","tiles","players", function($scope, tiles, players) {
+	$scope.tiles = tiles;
+	$scope.players = players;
+	$scope.addValue = function(tileNumber) {
+		// but why ? 
+		tileNumber = parseInt(tileNumber);
+		if ( tiles[tileNumber].player == -1 ) {
+			$scope.tiles[tileNumber].player =  1;
+			//@ TO-DO : throw event to line 31
 		}
 	}
+}]);
+
+game.directive("tvalue", function(){
+	return  {
+		restrict: "A",
+		// cacatul asta imi da eroare in consola daca las {{ tile.number }} ..
+		// daca nu folosesc {{ }} nu da eroare dar nu se inlocuieste parametrul cu poprietatea obiectului
+    	template: '<span class="size" ng-click="addValue(tile.number)" > {{  players.totalPlayers[tile.player].playerSimbol  }}</span>'
+	}
 });
-
-
-
-
